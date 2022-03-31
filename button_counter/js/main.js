079a9e46cd23d54c4ae91c6ecc7e98c45053874e
elspgoco0915 <elspgoco0915@gmail.com>
2022-03-31 19:07:12 +0900

    学習内容追加

---
+++ button_counter/js/main.js
@@ -0,0 +1,26 @@
+const buttonCounter = {
+  template: '<div><span>count: </span><button v-on:click="countUp"> {{ count }} </button></div>',
+  data: () => ({
+    count: 0
+  }),
+  methods: {
+    countUp: function(event) {
+      this.count++
+    }
+  }
+}
+
+const app = Vue.createApp({
+  data: () => ({
+
+  }),
+  components: {
+    'button-counter': buttonCounter
+  }
+})
+
+// app.component('hello-component', {
+//   template: '<p>Hello!</p>'
+// })
+
+app.mount('#app');
--- practice/js/main.js
+++ practice/js/main.js
@@ -1,18 +1,6 @@
 const app = Vue.createApp({
   data: () => ({
-    km: 0,
-    m: 0
-  }),
-  watch: {
-    km: function (value) {
-      console.log(value);
-      this.km = value
-      this.m = value * 1000
-    },
-    m : function(value) {
-      this.km = value / 1000
-      this.m = value
-    }
-  }
-});
+    show: false
+  })
+})
 app.mount('#app');
